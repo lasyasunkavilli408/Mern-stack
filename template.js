94fb528495c59e87f3c614d0a6c246d8affabf77
lasyasunkavilli408 <lasyasunkavilli2006@gmail.com>
2026-03-04 14:43:51 +0530

    Create template.js

---
+++ template.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+//Set EJS as template engine
+app.set("view engine", "ejs");
+
+//Optional: Set views folder manually
+app.set("views", path.join(__dirname, "views"));
+
+//Route
+app.get("/", (req, res) => {
+	const user = "Lasya";
+	const marks = 95;
+
+	res.render("home", {
+		name: user,
+		score: marks
+	});	
+});
+
+//Start server
+app.listen(3001, () => {
+	console.log("Server running on http://localhost:3001");
+});
